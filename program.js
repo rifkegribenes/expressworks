94a8ace80eee33e01edd21a7d05b025e8c0e3ad5
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 19:39:36 +0000

    exercise 5

--- program.js
+++ program.js
@@ -1,9 +1,6 @@
-var bodyparser = require('body-parser')
 var express = require('express')
 var app = express()
 
-app.use(bodyparser.urlencoded({extended: false}))
-app.post('/form', function(req, res) {
-  res.send(req.body.str.split('').reverse().join(''));
-})
+app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
 app.listen(process.argv[2]) 
