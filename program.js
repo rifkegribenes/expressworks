db3096f95bb5ae5e19017df95f5a0510d9143e7b
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 19:30:26 +0000

    exercise 4

--- program.js
+++ program.js
@@ -1,10 +1,9 @@
-var path = require('path')
+var bodyparser = require('body-parser')
 var express = require('express')
 var app = express()
 
-app.set('views', process.argv[3])
-app.set('view engine', 'jade')
-app.get('/home', function(req, res) {
-  res.render('index', {date: new Date().toDateString()})
+app.use(bodyparser.urlencoded({extended: false}))
+app.post('/form', function(req, res) {
+  res.send(req.body.str.split('').reverse().join(''));
 })
 app.listen(process.argv[2]) 
