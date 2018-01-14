21b034bcd3bd6042ddc1e46cc13669078657dce0
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 00:39:14 +0000

    exercise 3

--- program.js
+++ program.js
@@ -2,6 +2,9 @@ var path = require('path')
 var express = require('express')
 var app = express()
 
-app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
-
-app.listen(process.argv[2])
+app.set('views', process.argv[3])
+app.set('view engine', 'jade')
+app.get('/home', function(req, res) {
+  res.render('index', {date: new Date().toDateString()})
+})
+app.listen(process.argv[2]) 
