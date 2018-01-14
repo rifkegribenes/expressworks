c717f96d755bac000fb58170c342a882f3e4ca9d
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 00:27:56 +0000

    exercise 2

--- program.js
+++ program.js
@@ -1,5 +1,7 @@
-var app = require('express')();
-    app.get('/home', function(req, res) {
-      res.end('Hello World!')
-    })
-    app.listen(process.argv[2])
+var path = require('path')
+var express = require('express')
+var app = express()
+
+app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+
+app.listen(process.argv[2])
