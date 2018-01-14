e1fb24b1b0f4ba4e64cea3205e42242d06972a6a
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 19:57:56 +0000

    exercise 6

--- program.js
+++ program.js
@@ -1,6 +1,11 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
-app.use(require('stylus').middleware(process.argv[3]));
-app.use(express.static(process.argv[3]));
-app.listen(process.argv[2]) 
+app.put('/message/:id', (req, res) => {
+  const id = req.params.id
+  const str = require('crypto')
+    .createHash('sha1')
+    .update(new Date().toDateString() + id)
+    .digest('hex')
+  res.send(str)
+}).listen(process.argv[2]) 
